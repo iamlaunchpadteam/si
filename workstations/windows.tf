3a0d0e3a618b9e7d64767e1fa0ac6486cb5a0740
Kyree <x@y.com>
2022-11-07 12:27:42 -0800

    force windows instances to use specified hostname

--- workstations/windows.tf
+++ workstations/windows.tf
@@ -8,12 +8,21 @@ data "aws_ami" "windows_ami" {
 }
 
 
+
 resource "aws_instance" "windows" { 
   subnet_id            = var.target_subnet
   vpc_security_group_ids = var.vpc_security_group_ids
   ami           = data.aws_ami.windows_ami.id
   instance_type = "t3.micro"
+  user_data = <<EOF
+    <powershell>
+    # Rename Machine
+    Rename-Computer -NewName windows-instance-"${each.key}" -Force;
 
+    # Restart machine
+    shutdown -r -t 10;
+    </powershell>
+    EOF
   for_each = toset(["one", "two"])
   key_name = var.key_pair_name
 
